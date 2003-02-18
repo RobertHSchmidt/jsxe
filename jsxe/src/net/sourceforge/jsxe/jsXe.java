e5e3c2d70b9fe06e6de5ecd9d4ba17979c31294f
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2003-02-18 01:53:17 +0000

    removed unnessary method getStartingSize()

--- jsxe/src/net/sourceforge/jsxe/jsXe.java
+++ jsxe/src/net/sourceforge/jsxe/jsXe.java
@@ -269,11 +269,6 @@ public class jsXe {
         return documents;
     }//}}}
     
-    //this is bad implementation. It will be changed... eventually.
-    public static Dimension getStartingSize() {//{{{
-        return new Dimension(windowWidth, windowHeight);
-    }//}}}
-    
     public static ImageIcon getIcon() {//{{{
         return jsXeIcon;
     }//}}}
