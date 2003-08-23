972f389c26a76a53f1936eb75ec5e3038a6cd9f2
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2003-08-23 22:44:41 +0000

    Added utility scripts to run jsxe.

---
+++ trunk/jsxe/bin/jsXe.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+set INSTALLDIR=.
+set CLASSPATH=%INSTALLDIR%/jsXe.jar;%INSTALLDIR%/lib/xml-apis.jar;%INSTALLDIR%/lib/xercesImpl.jar
+set JSXE=net.sourceforge.jsxe.jsXe
+set JAVA_HEAP_SIZE=32
+java -mx%JAVA_HEAP_SIZE%m -cp %CLASSPATH% %JSXE% %1 %2 %3 %4 %5 %6 %7 %8 %9
+if errorlevel = 1
+pause
