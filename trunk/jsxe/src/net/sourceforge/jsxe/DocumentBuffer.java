bebc2117e70f084c0a38caf90cd9f8d37c80bf7d
rbilel <rbilel@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2003-12-06 19:42:48 +0000

    Fixed a "bug": Before: jsXe tries to saveAs an already opened document (isUntitled was 		returning true in the save action even if the document is not untiltled
    After: jsXe does not try to saveAs an already opened/saved document.

--- trunk/jsxe/src/net/sourceforge/jsxe/DocumentBuffer.java
+++ trunk/jsxe/src/net/sourceforge/jsxe/DocumentBuffer.java
@@ -167,6 +167,7 @@ public class DocumentBuffer {
                 setName(file.getName());
             }
             
+            m_file = file;
             setDirty(false);
             
             fireBufferSaved();
