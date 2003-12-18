386f8e9322a388429c0ad225658be9b75a898a36
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2003-12-18 20:48:11 +0000

    Added javadoc

--- trunk/jsxe/src/net/sourceforge/jsxe/dom/AdapterNode.java
+++ trunk/jsxe/src/net/sourceforge/jsxe/dom/AdapterNode.java
@@ -56,13 +56,40 @@ import org.w3c.dom.NodeList;
 
 //}}}
 
+/**
+ * <p>The AdapterNode class is meant to provide extensions to the W3C Node
+ * interface by wrapping around existing nodes created after parsing a document.
+ * It provides some event functionality and some methods for editing nodes in
+ * a DOM tree.</p>
+ * 
+ * @author Ian Lewis (<a href="mailto:IanLewis@member.fsf.org">IanLewis@member.fsf.org</a>)
+ * @version $Id$
+ */
 public class AdapterNode {
     
+    /**
+     * <p>Creates an AdapterNode for a root document node. This is normally used
+     * by an implementation of the XMLDocument interface when it is created.</p>
+     * @param xmlDocument the XMLDocument object that wraps the Document object
+     * @param document the document object that this AdapterNode is to
+     *                 represent
+     */
     AdapterNode(XMLDocument xmlDocument, Document document) {//{{{
         domNode = document;
         rootDocument = xmlDocument;
     }//}}}
     
+    /**
+     * <p>Creates a new AdapterNode for a node in a DOM tree. This is normally used
+     * by an implementation of the XMLDocument interface. Use the
+     * <code>newAdapterNode()</code> method in the XMLDocument interface to
+     * create AdapterNodes.</p>
+     * @param xmlDocument the XMLDocument that owns this node
+     * @param parent the parent AdapterNode object for the parent DOM node
+     * @param node the Node object that this AdapterNode represents. This node
+     *             should be a child of the Node that is wrapped by the parent
+     *             AdapterNode
+     */
     AdapterNode(XMLDocument xmlDocument, AdapterNode parent, Node node) {//{{{
         domNode = node;
         setParent(parent);
@@ -87,6 +114,12 @@ public class AdapterNode {
         return s;
     }//}}}
     
+    /**
+     * <p>Returns the index of the given AdapterNode if it is a child.</p>
+     * @param child the child node of this node
+     * @return the index where the child is located. -1 if the AdapterNode is
+     *         not a child
+     */
     public int index(AdapterNode child) {//{{{
         int count = childCount();
         for (int i=0; i<count; i++) {
@@ -98,10 +131,10 @@ public class AdapterNode {
     }//}}}
     
     /**
-     * Gets the child node at the given index.
-     * @param index the index of the requested node.
+     * <p>Gets the child node at the given index.</p>
+     * @param index the index of the requested node
      * @return an AdapterNode representing the node at the given index,
-     *         null if the index is out of bounds.
+     *         null if the index is out of bounds
      */
     public AdapterNode child(int index) {//{{{
         /*
@@ -135,14 +168,29 @@ public class AdapterNode {
        return child;
     }//}}}
     
+    /**
+     * <p>Gets the number of children that this node has.</p>
+     * @return the number of children of this node
+     */
     public int childCount() {//{{{
         return domNode.getChildNodes().getLength();
     }//}}}
     
+    /**
+     * <p>Gets the name of this node.</p>
+     * @return the name of the node
+     */
     public String getNodeName() {//{{{
         return domNode.getNodeName();
     }//}}}
     
+    /**
+     * <p>Sets the name of the node. Only element nodes are currently
+     * supported.</p>
+     * @param newValue the new name for this node
+     * @throws DOMException INVALID_CHARACTER_ERR: Raised if the specified name
+     *                      contains an illegal character.
+     */
     public void setNodeName(String newValue) throws DOMException {//{{{
         if (domNode.getNodeType() == Node.ELEMENT_NODE) {
             //Verify that this really is a change
@@ -176,10 +224,23 @@ public class AdapterNode {
         }
     }//}}}
     
+    /**
+     * <p>Gets the current value if this node.</p>
+     * @return the current value associated with this node
+     */
     public String getNodeValue() {//{{{
         return domNode.getNodeValue();
     }//}}}
     
+    /**
+     * <p>Sets the value of the node.</p>
+     * @param str the new value of the node
+     * @throws DOMException NO_MODIFICATION_ALLOWED_ERR: Raised when the node is
+     *                      readonly.
+     * @throws DOMException DOMSTRING_SIZE_ERR: Raised when it would return
+     *                      more characters than fit in a DOMString variable on
+     *                      the implementation platform.
+     */
     public void setNodeValue(String str) throws DOMException {//{{{
         // Make sure there is a change.
         if (str != null && !str.equals(domNode.getNodeValue())) {
@@ -188,18 +249,43 @@ public class AdapterNode {
         }
     }//}}}
     
+    /**
+     * <p>Gets the type of the node specified in the W3C Node interface.</p>
+     * @return the node type
+     */
     public short getNodeType() {//{{{
         return domNode.getNodeType();
     }//}}}
     
+    /**
+     * <p>Gets the parent AdapterNode object.</p>
+     * @return the AdapterNode that is the parent of this node
+     */
     public AdapterNode getParentNode() {//{{{
         return parentNode;
     }//}}}
     
+    /**
+     * <p>Gets the attributes associated with this node.</p>
+     * @return a map of the attributes associated with this node.
+     *         <code>null</code> if this is not an element node
+     */
     public NamedNodeMap getAttributes() {//{{{
         return domNode.getAttributes();
     }//}}}
     
+    /**
+     * <p>Adds a new child to this node given the node name, value, and type.</p>
+     * @param name the name of the new child node
+     * @param value the value of the new child node
+     * @param type the type of the new child node as specified by the W3C Node
+     *             interface
+     * @return the new child that was created
+     * @throws DOMException INVALID_CHARACTER_ERR: Raised if the specified name
+     *                      or value contains an illegal character.
+     *                      NOT_SUPPORTED_ERR: Raised if the node type is not
+     *                      supported.
+     */
     public AdapterNode addAdapterNode(String name, String value, short type) throws DOMException {//{{{
         
         Node newNode = null;
@@ -242,6 +328,11 @@ public class AdapterNode {
         return newAdapterNode;
     }//}}}
     
+    /**
+     * <p>Adds an already existing AdapterNode to this node as a child.</p>
+     * @param node the node to be added.
+     * @return a reference to the node that was added.
+     */
     public AdapterNode addAdapterNode(AdapterNode node) {//{{{
         //add to this AdapterNode and the DOM.
         if (node != null) {
@@ -262,6 +353,14 @@ public class AdapterNode {
    //     fireNodeRemoved(this, this);
    // }//}}}
     
+    /**
+     * <p>Removes a child from this node.</p>
+     * @param child the child node to remove from this node
+     * @throws DOMException NO_MODIFICATION_ALLOWED_ERR: Raised if this node is
+     *                      readonly.
+     * @throws DOMException NOT_FOUND_ERR: Raised if oldChild is not
+     *                      a child of this node.
+     */
     public void remove(AdapterNode child) throws DOMException {//{{{
         if (child != null) {
             domNode.removeChild(child.getNode());
@@ -270,6 +369,18 @@ public class AdapterNode {
         }
     }//}}}
     
+    /**
+     * <p>Sets an attribute of this node. If the specified attribute does not
+     * exist it is created.</p>
+     * @param name the name of the attribute
+     * @param value the new value of the attribute
+     * @throws DOMException NOT_SUPPORTED_ERR: if this is not an element node
+     * @throws DOMException INVALID_CHARACTER_ERR: Raised if the specified
+     *                      qualified name contains an illegal character, per
+     *                      the XML 1.0 specification
+     * @throws DOMException NO_MODIFICATION_ALLOWED_ERR: Raised if this node is
+     *                      readonly
+     */
     public void setAttribute(String name, String value) throws DOMException {//{{{
         if (domNode.getNodeType() == Node.ELEMENT_NODE) {
             Element element = (Element)domNode;
@@ -280,6 +391,11 @@ public class AdapterNode {
         }
     }//}}}
     
+    /**
+     * <p>Gets the value of an attribute associated with this node.</p>
+     * @param name the name of the attribute
+     * @throws DOMException NOT_SUPPORTED_ERR: if this is not an element node
+     */
     public String getAttribute(String name) throws DOMException {//{{{
         if (domNode.getNodeType() == Node.ELEMENT_NODE) {
             Element element = (Element)domNode;
@@ -289,6 +405,14 @@ public class AdapterNode {
         }
     }//}}}
     
+    /**
+     * <p>Removes an attribute at the given index.</p>
+     * <p><b>Note:</b> Attributes are sorted alphabetically.</p>
+     * @param index the index of the node to remove
+     * @throws DOMException NOT_SUPPORTED_ERR: if this is not an element node
+     * @throws DOMException NO_MODIFICATION_ALLOWED_ERR: Raised if this node is
+     *                      readonly
+     */
     public void removeAttributeAt(int index) throws DOMException {//{{{
         if (domNode.getNodeType() == Node.ELEMENT_NODE) {
             Element element = (Element)domNode;
@@ -301,6 +425,13 @@ public class AdapterNode {
         }
     }//}}}
     
+    /**
+     * <p>Removes an attribute by name.</p>
+     * @param attr the name of the attribute to remove
+     * @throws DOMException NOT_SUPPORTED_ERR: if this is not an element node
+     * @throws DOMException NO_MODIFICATION_ALLOWED_ERR: Raised if this node is
+     *                      readonly
+     */
     public void removeAttribute(String attr) throws DOMException {//{{{
         if (domNode.getNodeType() == Node.ELEMENT_NODE) {
             Element element = (Element)domNode;
@@ -311,6 +442,12 @@ public class AdapterNode {
         }
     }//}}}
     
+    /**
+     * <p>Gets the value of an attribute at the given index</p>
+     * <p><b>Note:</b> Attributes are sorted alphabetically.</p>
+     * @param index the index of the attribute to get
+     * @throws DOMException NOT_SUPPORTED_ERR: if this is not an element node
+     */
     public String getAttributeAt(int index) throws DOMException {//{{{
         if (domNode.getNodeType() == Node.ELEMENT_NODE) {
             Element element = (Element)domNode;
@@ -322,24 +459,46 @@ public class AdapterNode {
         }
     }//}}}
     
+    /**
+     * <p>Compares this AdapterNode with another.</p>
+     * @param node the AdapterNode to compare to
+     * @return true if the underlying Node object for the two AdapterNodes is
+     *         is the same.
+     */
     public boolean equals(AdapterNode node) {//{{{
         return node.getNode() == domNode;
     }//}}}
     
+    /**
+     * <p>Adds an AdapterNodeListener to be notified when this node changes</p>
+     * @param listener the listener to add
+     */
     public void addAdapterNodeListener(AdapterNodeListener listener) {//{{{
         listeners.add(listener);
     }//}}}
     
+    /**
+     * <p>Removes a listener from this node if it exists</p>
+     * @param listener the listener to remove
+     */
     public void removeAdapterNodeListener(AdapterNodeListener listener) {//{{{
         listeners.remove(listeners.indexOf(listener));
     }//}}}
     
     //{{{ Protected members
     
+    /**
+     * <p>Gets the underlying Node object that this AdapterNode wraps.</p>
+     * @return the underlying Node object for this AdapterNode object
+     */
     protected Node getNode() {//{{{
         return domNode;
     }//}}}
     
+    /**
+     * <p>Sets the parent node of this AdapterNode.</p>
+     * @param parent the new parent for this AdapterNode
+     */
     protected void setParent(AdapterNode parent) {//{{{
         parentNode = parent;
     }//}}}
