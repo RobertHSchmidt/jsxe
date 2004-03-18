1c2b73960928b373cac98244d4f73cf650d64cb8
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2004-03-18 14:12:57 +0000

    Added transferrable node for dnd and removed DefaultXMLDocument since it's old

---
+++ trunk/jsxe/src/net/sourceforge/jsxe/gui/TransferableNode.java
@@ -0,0 +1,111 @@
+/*
+TransferableNode.java
+:tabSize=4:indentSize=4:noTabs=true:
+:folding=explicit:collapseFolds=1:
+
+jsXe is the Java Simple XML Editor
+jsXe is a gui application that can edit an XML document and create a tree view.
+The user can then edit this tree and the content in the tree and save the
+document.
+
+This file contains the TransferableNode class which is used for
+data transfer of XML nodes.
+
+This file written by Ian Lewis (IanLewis@member.fsf.org)
+Copyright (C) 2002 Ian Lewis
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+Optionally, you may find a copy of the GNU General Public License
+from http://www.fsf.org/copyleft/gpl.txt
+*/
+
+package net.sourceforge.jsxe.gui;
+
+//{{{ imports
+
+//{{{ jsXe classes
+import net.sourceforge.jsxe.jsXe;
+import net.sourceforge.jsxe.dom.AdapterNode;
+//}}}
+
+//{{{ AWT classes
+import java.awt.datatransfer.*;
+//}}}
+
+//{{{ Java classes
+import java.io.IOException;
+import java.util.*;
+//}}}
+
+//}}}
+
+public class TransferableNode implements Transferable {
+    
+    //{{{ TransferableNode constructor
+    
+    public TransferableNode(AdapterNode node) {
+        m_node = node;
+    }//}}}
+    
+    //{{{ getTransferDataFlavors()
+    
+    public synchronized DataFlavor[] getTransferDataFlavors() {
+        return flavors;
+    }//}}}
+    
+    //{{{ isDataFlavorSupported()
+    
+    public boolean isDataFlavorSupported(DataFlavor flavor) {
+        return (flavorList.contains(flavor));
+    }//}}}
+    
+    //{{{ getTransferData()
+    
+    public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+        if (stringFlavor.equals(flavor)) {
+            return m_node.serializeToString();
+        } else {
+            if (nodeFlavor.equals(flavor)) {
+                return m_node;
+            } else {
+                throw new UnsupportedFlavorException(flavor);
+            }
+        }
+    }//}}}
+    
+    //{{{ Private Members
+    public static final DataFlavor stringFlavor = DataFlavor.stringFlavor;
+    public static final DataFlavor nodeFlavor;
+    
+    static {
+        DataFlavor flav = null;
+        try {
+            flav = new DataFlavor(Class.forName("net.sourceforge.jsxe.dom.AdapterNode"), "XML Node");
+        } catch (ClassNotFoundException e) {
+            jsXe.exiterror(null, e.getMessage(), 1);
+        }
+        nodeFlavor = flav;
+    }
+    
+    private static final DataFlavor[] flavors = {
+        stringFlavor,
+        nodeFlavor
+    };
+
+    private static final List flavorList = Arrays.asList( flavors );
+    private AdapterNode m_node;
+    //}}}
+    
+}
