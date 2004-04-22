448205da1543276340e9f6dd850e7e8ae1438b2e
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2004-04-22 18:12:33 +0000

    Updated how properties are loaded

--- trunk/jsxe/src/net/sourceforge/jsxe/jsXe.java
+++ trunk/jsxe/src/net/sourceforge/jsxe/jsXe.java
@@ -520,19 +520,35 @@ public class jsXe {
         return props.setProperty(key, value);
     }//}}}
     
+    //{{{ getDefaultProperty()
+    /**
+     * Gets a default global property. Returns null if there is no default
+     * property for the given key.
+     * 
+     */
+    public static final String getDefaultProperty(String key) {
+        return defaultProps.getProperty(key);
+    }
+    
+    //}}}
+    
     //{{{ getProperty()
     /**
-     * Gets a jsXe global property. Returns null if the property is not found.
+     * Gets a jsXe global property. This returns a user defined value for the
+     * property, the default value of the property if no user defined value is
+     * found or null if neither exist.
      * @param key The key of the property to get.
      * @return The value associated with the key or null if the key is not found.
      */
     public static final String getProperty(String key) {
-        return props.getProperty(key, defaultProps.getProperty(key));
+        return getProperty(key, null);
     }//}}}
     
     //{{{ getProperty()
     /**
-     * Gets a jsXe global property specifying a default value.
+     * * Gets a jsXe global property. This returns a user defined value for the
+     * property, the default value of the property if no user defined value is
+     * found or the default value given if neither exist.
      * @param key The key of the property to get.
      * @param defaultValue The default value to return when the key is not found.
      * @return The value associated with the key or the default value if the key is not found.
