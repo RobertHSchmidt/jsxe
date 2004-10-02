d76d99e21a28d4a9259d5c0f6ba08efb9d93694a
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2004-10-02 05:19:36 +0000

    added support for properties for the new options dialog

--- jsxe/src/net/sourceforge/jsxe/jsXe.java
+++ jsxe/src/net/sourceforge/jsxe/jsXe.java
@@ -651,6 +651,39 @@ public class jsXe {
 		return props.getProperty(key, defaultProps.getProperty(key, defaultValue));
 	} //}}}
     
+    //{{{ getIntegerProperty()
+    /**
+	 * Returns the value of an integer property.
+	 * @param name The property
+	 * @param def The default value
+	 * @since jsXe 0.2 pre24
+	 */
+    public static final int getIntegerProperty(String key, int defaultValue) {
+        int intValue = defaultValue;
+        String value = getProperty(key);
+		if (value == null) {
+			return defaultValue;
+        } else {
+            try {
+				return Integer.parseInt(value.trim());
+            } catch(NumberFormatException nf) {
+                return defaultValue;
+            }
+        }
+    }//}}}
+    
+    //{{{ setIntegerProperty() method
+	/**
+	 * Sets the value of an integer property.
+	 * @param name The property
+	 * @param value The value
+	 * @since jsXe 0.2 pre24
+	 */
+	public static final void setIntegerProperty(String name, int value)
+	{
+		setProperty(name, String.valueOf(value));
+	} //}}}
+    
     //{{{ addActionSet()
     
     public static void addActionSet(ActionSet set) {
