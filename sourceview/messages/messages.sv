9d2434d91427beedde282f874d85e49bbd2461b2
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-02-02 20:52:58 +0000

    added new localized messages files. jsXe now supports plugins having their own localized messages.

---
+++ sourceview/messages/messages.sv
@@ -0,0 +1,12 @@
+# JSXE Swedish properties file
+# $Id$
+# Currently maintained by Patrik Johansson <patjoh@itstud.chalmers.se>
+#:mode=properties:
+#:tabSize=4:indentSize=4:noTabs=true:
+#:folding=explicit:collapseFolds=1:
+
+SourceView.Cut=Klipp ut
+SourceView.Copy=Kopiera
+SourceView.Paste=Klistra in
+SourceView.Find=Sök...
+SourceView.FindNext=Sök nästa
