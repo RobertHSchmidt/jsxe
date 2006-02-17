9d66cff6c3bc2e39a2b8e68ec38f634fde4f6b02
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-02-17 22:51:04 +0000

    bug: 1433838 java.lang.IndexOutOfBoundsException when saving fixed a bug where indeces were not being kept properly after a document was bigger than the write buffer size

--- trunk/jsxe/bin/jsXe.bat
+++ trunk/jsxe/bin/jsXe.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 set JSXEDIR=.
-set CLASSPATH=%JSXEDIR%/jsXe.jar;%JSXEDIR%/lib/xml-apis.jar;%JSXEDIR%/lib/xercesImpl.jar;%JSXEDIR%/lib/xml-commons-resolver.jar
+set CLASSPATH=%JSXEDIR%/jsXe.jar;%JSXEDIR%/lib/xml-apis.jar;%JSXEDIR%/lib/xercesImpl.jar;%JSXEDIR%/lib/resolver.jar
 set JSXE=net.sourceforge.jsxe.jsXe
 set JAVA_HEAP_SIZE=32
 java -mx%JAVA_HEAP_SIZE%m -cp %CLASSPATH% %JSXE% %1 %2 %3 %4 %5 %6 %7 %8 %9
