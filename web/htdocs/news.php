8db0d14e52a5ae364de99839508293cf348ce938
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-04-30 05:13:34 +0000

    updated to php

---
+++ web/htdocs/news.php
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>jsXe: The Java Simple XML Editor</title>
+    <meta content="Generaol" name="rating"/>
+    <meta content="Index, follow" name="robots"/>
+    <meta content="text/html; charset=UTF-8" http-equiv="Content-type"/>
+    <link href="mailto:IanLewis@member.fsf.org" rev="made"/>
+    <meta content="Ian Lewis" name="author"/>
+    <meta content="jsXe, Java, XML, Editor, Sourceforge, GNU" name="keywords"/>
+    <meta content="Information and links about jsXe" name="description"/>
+    <link rel="stylesheet" type="text/css" media="all" href="css/main.css" />
+  </head>
+  <body>
+    <div id="title"><h1>jsXe: The Java Simple XML Editor</h1></div>
+    
+    <div id="sidebar">
+      <?php include("sidebar.php") ?>
+    </div>
+    <div id="mainBody">
+      
+      <h2>News</h2>
+      
+      <?php include("news.cache") ?>
+      
+      <div id="images">
+        <a href="http://sourceforge.net/"><img alt="SourceForge Logo" src="http://sourceforge.net/sflogo.php?group_id=58584"/></a>
+        <a href="http://validator.w3.org/check/referer"><img alt="Valid XHTML 1.0 Transitional" src="http://www.w3.org/Icons/valid-xhtml10"/></a>
+        <a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fjsxe.sourceforge.net%2Fcss%2Fmain.css&amp;usermedium=all"><img alt="Valid CSS" src="http://jigsaw.w3.org/css-validator/images/vcss"/></a>
+        <a href="http://www.jedit.org"><img alt="Made with jEdit" src="images/made-with-jedit-6.png"/></a>
+      </div>
+    </div>
+  </body>
+</html>
