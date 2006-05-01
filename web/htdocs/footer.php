aab21e862bf82a19657b76a6a845d484197584a3
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-05-01 03:43:31 +0000

    moved the footer to it's own file.

---
+++ web/htdocs/footer.php
@@ -0,0 +1,6 @@
+<div id="images">
+  <a href="http://sourceforge.net/"><img alt="SourceForge Logo" src="http://sourceforge.net/sflogo.php?group_id=58584"/></a>
+  <a href="http://validator.w3.org/check/referer"><img alt="Valid XHTML 1.0 Transitional" src="http://www.w3.org/Icons/valid-xhtml10"/></a>
+  <a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fjsxe.sourceforge.net%2Fcss%2Fmain.css&amp;usermedium=all"><img alt="Valid CSS" src="http://jigsaw.w3.org/css-validator/images/vcss"/></a>
+  <a href="http://www.jedit.org"><img alt="Made with jEdit" src="images/made-with-jedit-6.png"/></a>
+</div>
