cfb7d2fec2571b307e952840fa686bef52c4b669
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-05-17 04:18:29 +0000

    fixed the 'any browser' icon

--- web/htdocs/footer.php
+++ web/htdocs/footer.php
@@ -1,7 +1,7 @@
 <div id="images">
   <a href="http://validator.w3.org/check/referer"><img title="This site is valid XHTML." alt="Valid XHTML 1.0 Transitional" src="http://www.ianlewis.org/images/xhtml.png"/></a>
   <a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fjsxe.sourceforge.net%2Fcss%2Fmain.css&amp;usermedium=all"><img title="This site is valid CSS." alt="Valid CSS" src="http://www.ianlewis.org/images/css.png"/></a>
-  <img title="This site is usable in any web browser." alt="Any Browser!" "http://www.ianlewis.org/images/anybrowser.png"/>
+  <img title="This site is usable in any web browser." alt="Any Browser!" src="http://www.ianlewis.org/images/anybrowser.png"/>
   <!--<a href="http://sourceforge.net/"><img alt="SourceForge Logo" src="http://sourceforge.net/sflogo.php?group_id=58584"/></a>
   <a href="http://validator.w3.org/check/referer"><img alt="Valid XHTML 1.0 Transitional" src="http://www.w3.org/Icons/valid-xhtml10"/></a>
   <a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fjsxe.sourceforge.net%2Fcss%2Fmain.css&amp;usermedium=all"><img alt="Valid CSS" src="http://jigsaw.w3.org/css-validator/images/vcss"/></a>
