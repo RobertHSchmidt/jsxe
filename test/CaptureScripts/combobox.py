122f0363e35fd2c0498cceb5c838f14a2ba33774
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-05-25 00:50:47 +0000

    creating a branch for the new data model

--- test/CaptureScripts/combobox.py
+++
@@ -1,9 +0,0 @@
-#:tabSize=4:indentSize=4:noTabs=false:
-#:folding=explicit:collapseFolds=1:
-
-#a hack since the select() function doesn't seem to save the value
-#works in real life though.
-def setValue(name, item):
-	from marathon.playback import *
-	combobox = getComponent(name)
-	combobox.setSelectedItem(item)
