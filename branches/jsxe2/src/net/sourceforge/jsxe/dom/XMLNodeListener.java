15cd29c15a2c8ed7f08fd1dd3da381719240f2f0
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-06-04 01:53:46 +0000

    Added the XMLNodeListener class for use in notifying objects concerning changes to nodes Updated the XMLNode class to have a simple implementation of an XML Node editing interface AdapterNode and AdapterNodeListener are now deprecated Updated the Changelog

---
+++ branches/jsxe2/src/net/sourceforge/jsxe/dom/XMLNodeListener.java
@@ -0,0 +1,39 @@
+/*
+XMLNodeListener.java
+:tabSize=4:indentSize=4:noTabs=true:
+:folding=explicit:collapseFolds=1:
+
+Copyright (C) 2006 Ian Lewis (IanLewis@member.fsf.org)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+Optionally, you may find a copy of the GNU General Public License
+from http://www.fsf.org/copyleft/gpl.txt
+*/
+
+package net.sourceforge.jsxe.dom;
+
+/**
+ * XMLNodeListener is used to notify objects of a change to a specific XMLNode.
+ * These changes can occur via changes to the text or document structure. Both
+ * are maintained by the XMLDocument class.
+ *
+ * @author Ian Lewis (<a href="mailto:IanLewis@member.fsf.org">IanLewis@member.fsf.org</a>)
+ * @version $Id$
+ * @since jsXe 0.5 pre1
+ * @see XMLDocument
+ */
+public interface XMLNodeListener {
+    
+}
