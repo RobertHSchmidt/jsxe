94e9c4de4bf67d12ece3c0f6be1c4a13e0488da2
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-06-07 03:32:15 +0000

    Adding new files for the Document Options Dialog

---
+++ jsxe/src/net/sourceforge/jsxe/action/DocumentOptionsAction.java
@@ -0,0 +1,78 @@
+/*
+DocumentOptionsAction.java
+:tabSize=4:indentSize=4:noTabs=true:
+:folding=explicit:collapseFolds=1:
+
+Copyright (C) 2006 Ian Lewis (IanLewis@member.fsf.org)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+Optionally, you may find a copy of the GNU General Public License
+from http://www.fsf.org/copyleft/gpl.txt
+*/
+
+package net.sourceforge.jsxe.action;
+
+//{{{ imports
+/*
+All classes are listed explicitly so
+it is easy to see which package it
+belongs to.
+*/
+
+//{{{ jsXe classes
+import net.sourceforge.jsxe.gui.DocumentOptionsDialog;
+import net.sourceforge.jsxe.gui.Messages;
+import net.sourceforge.jsxe.gui.TabbedView;
+//}}}
+
+//{{{ Swing components
+import javax.swing.Action;
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+//}}}
+
+//{{{ AWT components
+import java.awt.event.ActionEvent;
+//}}}
+
+//}}}
+
+/**
+ * The action that is executed when the user selects 'Document Options' from
+ * the tools menu.
+ * @author Ian Lewis (<a href="mailto:IanLewis@member.fsf.org">IanLewis@member.fsf.org</a>)
+ * @version $Id$
+ */
+public class DocumentOptionsAction extends AbstractAction {
+    
+    //{{{ DocumentOptionsAction constructor
+    
+    public DocumentOptionsAction(TabbedView parent) {
+        //putValue(Action.NAME, "Options...");
+    	putValue(Action.NAME, Messages.getMessage("Tools.Document.Options"));	
+        putValue(Action.MNEMONIC_KEY, new Integer(KeyStroke.getKeyStroke("D").getKeyCode()));
+        view = parent;
+    }//}}}
+    
+    //{{{ actionPerformed()
+    
+    public void actionPerformed(ActionEvent e) {
+        new DocumentOptionsDialog(view);
+    }//}}}
+    
+    //{{{ Private members
+    private TabbedView view;
+    //}}}
+}
