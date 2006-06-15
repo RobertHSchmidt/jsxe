8d498241bc6b12e78b1e7354c7254971fe03aae3
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-06-15 21:37:39 +0000

    Removed getComponent since it's not needed

--- jsxe/src/net/sourceforge/jsxe/gui/GridPanel.java
+++ jsxe/src/net/sourceforge/jsxe/gui/GridPanel.java
@@ -63,15 +63,6 @@ public class GridPanel extends JPanel {
         setLayout(gridBag = new GridBagLayout());
     } //}}}
 
-    //{{{ getComponent() method
-    /**
-     * Returns the component that should be displayed for this option pane.
-     * Because this class extends Component, it simply returns "this".
-     */
-    public Component getComponent() {
-        return this;
-    } //}}}
-
     //{{{ addComponent() method
     /**
      * Adds a component to the option pane. Components are
