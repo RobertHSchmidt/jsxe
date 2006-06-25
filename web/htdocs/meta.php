29a5214310628e26ae1cf6ab95d3d873d6205e6b
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-06-25 17:42:18 +0000

    Added jsXe rss feeds

--- web/htdocs/meta.php
+++ web/htdocs/meta.php
@@ -7,3 +7,5 @@
 <link rel="Shortcut Icon" href="/favicon.ico" type="image/x-icon" />
 <link rel="stylesheet" type="text/css" media="all" href="css/main.css" />
 <link href="mailto:IanLewis@member.fsf.org" rev="made"/>
+<link rel="alternate" type="application/rss+xml" title="jsXe Project News" href="http://sourceforge.net/export/rss2_sfnews.php?group_id=1&amp;rss_fulltext=1" />
+<link rel="alternate" type="application/rss+xml" title="jsXe CVS Commits" href="https://cia.navi.cx/stats/project/jsxe/.rss"/>
