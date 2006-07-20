b78180d214c17b75df025f53777c83e7e1443d61
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-07-20 21:56:11 +0000

    Added an abstract character data node and CDATA section node

---
+++ src/net/sourceforge/jsxe/dom2/XMLCharacterData.java
@@ -0,0 +1,56 @@
+/*
+XMLCharacterData.java
+:tabSize=4:indentSize=4:noTabs=true:
+:folding=explicit:collapseFolds=1:
+
+Copyright (C) 2006 Ian Lewis (IanLewis@member.fsf.org)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+Optionally, you may find a copy of the GNU General Public License
+from http://www.fsf.org/copyleft/gpl.txt
+*/
+
+package net.sourceforge.jsxe.dom2;
+
+import org.w3c.dom.*;
+
+public abstract class XMLCharacterData extends XMLNode {
+    
+    //{{{ XMLCharacterData constructor
+    XMLCharacterData(CharacterData data) {
+        super(data);
+    }//}}}
+    
+    //{{{ appendData()
+    public void append(String data) throws DOMException {
+        ((CharacterData)getNode()).appendData(data);
+    }//}}}
+    
+    //{{{ deleteData()
+    public void deleteData(int offset, int len) throws DOMException {
+        ((CharacterData)getNode()).deleteData(offset, len);
+    }//}}}
+    
+    //{{{ insertData()
+    public void insertData(int offset, String data) {
+        ((CharacterData)getNode()).insertData(offset, data);
+    }//}}}
+    
+    //{{{ getLength()
+    public int getLength() {
+        return ((CharacterData)getNode()).getLength();
+    }//}}}
+    
+}
