bf0a572f1e15a96b8207aa03b1e90266438e3fac
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-08-07 21:53:57 +0000

    Backed out changes for undo in the source view

--- sourceview/src/sourceview/SourceViewPlugin.java
+++ sourceview/src/sourceview/SourceViewPlugin.java
@@ -65,8 +65,6 @@ public class SourceViewPlugin extends ViewPlugin {
         addAction(new EditPasteAction());
         addAction(new EditFindAction());
         addAction(new EditFindNextAction());
-        addAction(new UndoAction());
-        addAction(new RedoAction());
     }//}}}
     
     //{{{ newDocumentView()
