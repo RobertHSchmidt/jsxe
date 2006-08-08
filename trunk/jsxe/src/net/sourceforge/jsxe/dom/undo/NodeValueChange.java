3ea8461ddaf05891d641ec9751999085201b28fa
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-08-08 05:55:21 +0000

    Fixed a bug with drag and drop and undo

--- trunk/jsxe/src/net/sourceforge/jsxe/dom/undo/NodeValueChange.java
+++ trunk/jsxe/src/net/sourceforge/jsxe/dom/undo/NodeValueChange.java
@@ -50,9 +50,9 @@ import org.w3c.dom.DOMException;
  */
 public class NodeValueChange extends AbstractUndoableEdit {
     
-    AdapterNode m_node;
-    String m_oldValue;
-    String m_newValue;
+    private AdapterNode m_node;
+    private String m_oldValue;
+    private String m_newValue;
     
     //{{{ NodeValueChange constructor
     public NodeValueChange(AdapterNode node, String oldValue, String newValue) {
