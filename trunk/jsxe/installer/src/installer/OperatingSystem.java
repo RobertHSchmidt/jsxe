c89bf0d353a9c83114a6ed4f8060f44ff23ad07d
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-08-13 04:07:13 +0000

    merge from the 05pre3 branch

--- trunk/jsxe/installer/src/installer/OperatingSystem.java
+++ trunk/jsxe/installer/src/installer/OperatingSystem.java
@@ -22,321 +22,372 @@ import java.util.Vector;
  */
 public abstract class OperatingSystem
 {
-	public abstract String getInstallDirectory(String name, String version);
+    public abstract String getInstallDirectory(String name, String version);
 
-	public abstract static class OSTask
-	{
-		protected Install installer;
-		protected String name;
-		protected String label;
-		protected String directory;
-		protected boolean enabled;
+    public abstract static class OSTask
+    {
+        protected Install installer;
+        protected String name;
+        protected String label;
+        protected String directory;
+        protected boolean enabled;
 
-		public OSTask(Install installer, String name)
-		{
-			this.installer = installer;
-			this.name = name;
-			this.label = installer.getProperty("ostask." + name + ".label");
-			this.directory = getDefaultDirectory(installer);
+        public OSTask(Install installer, String name)
+        {
+            this.installer = installer;
+            this.name = name;
+            this.label = installer.getProperty("ostask." + name + ".label");
+            this.directory = getDefaultDirectory(installer);
 
-			// on by default
-			enabled = true;
-		}
+            // on by default
+            enabled = true;
+        }
 
-		public String getName()
-		{
-			return name;
-		}
+        public String getName()
+        {
+            return name;
+        }
 
-		public String getLabel()
-		{
-			return label;
-		}
+        public String getLabel()
+        {
+            return label;
+        }
 
-		public String getDefaultDirectory(Install installer)
-		{
-			return null;
-		}
+        public String getDefaultDirectory(Install installer)
+        {
+            return null;
+        }
 
-		public String getDirectory()
-		{
-			return directory;
-		}
+        public String getDirectory()
+        {
+            return directory;
+        }
 
-		public boolean isEnabled()
-		{
-			return enabled;
-		}
+        public boolean isEnabled()
+        {
+            return enabled;
+        }
 
-		public void setEnabled(boolean enabled)
-		{
-			this.enabled = enabled;
-		}
+        public void setEnabled(boolean enabled)
+        {
+            this.enabled = enabled;
+        }
 
-		public void setDirectory(String directory)
-		{
-			this.directory = directory;
-		}
+        public void setDirectory(String directory)
+        {
+            this.directory = directory;
+        }
 
-		public abstract void perform(String installDir,
-			Vector filesets) throws IOException;
-	}
+        public abstract void perform(String installDir,
+            Vector filesets) throws IOException;
+    }
 
-	public OSTask[] getOSTasks(Install installer)
-	{
-		return new OSTask[0];
-	}
+    public OSTask[] getOSTasks(Install installer)
+    {
+        return new OSTask[0];
+    }
 
-	public void mkdirs(String directory) throws IOException
-	{
-		File file = new File(directory);
-		if(!file.exists())
-			file.mkdirs();
-	}
+    public void mkdirs(String directory) throws IOException
+    {
+        File file = new File(directory);
+        if(!file.exists())
+            file.mkdirs();
+    }
 
-	public static OperatingSystem getOperatingSystem()
-	{
-		if(os != null)
-			return os;
+    public static OperatingSystem getOperatingSystem()
+    {
+        if(os != null)
+            return os;
 
-		if(System.getProperty("mrj.version") != null)
-			os = new MacOS();
-		else
-		{
-			String osName = System.getProperty("os.name");
-			if(osName.indexOf("Windows") != -1)
-				os = new Windows();
-			else if(osName.indexOf("OS/2") != -1)
-				os = new HalfAnOS();
-			else if(osName.indexOf("VMS") != -1)
-				os = new VMS();
-			else
-				os = new Unix();
-		}
+        if(System.getProperty("mrj.version") != null)
+            os = new MacOS();
+        else
+        {
+            String osName = System.getProperty("os.name");
+            if(osName.indexOf("Windows") != -1)
+                os = new Windows();
+            else if(osName.indexOf("OS/2") != -1)
+                os = new HalfAnOS();
+            else if(osName.indexOf("VMS") != -1)
+                os = new VMS();
+            else
+                os = new Unix();
+        }
 
-		return os;
-	}
+        return os;
+    }
 
-	public static class Unix extends OperatingSystem
-	{
-		public String getInstallDirectory(String name, String version)
-		{
-			String dir = "/usr/local/share/";
-			if(!new File(dir).canWrite())
-				dir = System.getProperty("user.home");
+    public static class Unix extends OperatingSystem
+    {
+        public String getInstallDirectory(String name, String version)
+        {
+            String dir = "/usr/local/share/";
+            if(!new File(dir).canWrite())
+                dir = System.getProperty("user.home");
 
-			return new File(dir,name.toLowerCase() + "/" + version).getPath();
-		}
+            return new File(dir,name.toLowerCase() + "/" + version).getPath();
+        }
 
-		public class ScriptOSTask extends OSTask
-		{
-			public ScriptOSTask(Install installer)
-			{
-				super(installer,"unix-script");
-			}
+        public class ScriptOSTask extends OSTask
+        {
+            public ScriptOSTask(Install installer)
+            {
+                super(installer,"unix-script");
+            }
 
-			public String getDefaultDirectory(Install installer)
-			{
-				String dir = "/usr/local/";
-				if(!new File(dir).canWrite())
-					dir = System.getProperty("user.home");
+            public String getDefaultDirectory(Install installer)
+            {
+                String dir = "/usr/local/";
+                if(!new File(dir).canWrite())
+                    dir = System.getProperty("user.home");
 
-				return new File(dir,"bin").getPath();
-			}
+                return new File(dir,"bin").getPath();
+            }
 
-			public void perform(String installDir,
-				Vector filesets) throws IOException
-			{
-				if(!enabled)
-					return;
+            public void perform(String installDir,
+                Vector filesets) throws IOException
+            {
+                if(!enabled)
+                    return;
 
-				mkdirs(directory);
+                mkdirs(directory);
 
-				String name = installer.getProperty("app.name");
+                String name = installer.getProperty("app.name");
+                
+                //jsxe specific: this is needed for java to load our xerces rather
+                // than the one included with java.
+                String vmArgs = "-Djava.endorsed.dirs=\""+installDir + File.separator+"lib\"";
 
-				// create app start script
-				String script = directory + File.separatorChar
-					+ name.toLowerCase();
+                // create app start script
+                String script = directory + File.separatorChar
+                    + name.toLowerCase();
 
-				// Delete existing copy
-				new File(script).delete();
+                // Delete existing copy
+                new File(script).delete();
 
-				// Write simple script
-				FileWriter out = new FileWriter(script);
-				out.write("#!/bin/sh\n");
-				out.write("# Java heap size, in megabytes\n");
-				out.write("JAVA_HEAP_SIZE=32\n");
-				out.write("DEFAULT_JAVA_HOME=\""
-					+ System.getProperty("java.home")
-					+ "\"\n");
-				out.write("if [ \"$JAVA_HOME\" = \"\" ]; then\n");
-				out.write("JAVA_HOME=\"$DEFAULT_JAVA_HOME\"\n");
-				out.write("fi\n");
+                // Write simple script
+                FileWriter out = new FileWriter(script);
+                out.write("#!/bin/sh\n");
+                out.write("# Java heap size, in megabytes\n");
+                out.write("JAVA_HEAP_SIZE=32\n");
+                out.write("DEFAULT_JAVA_HOME=\""
+                    + System.getProperty("java.home")
+                    + "\"\n");
+                out.write("if [ \"$JAVA_HOME\" = \"\" ]; then\n");
+                out.write("JAVA_HOME=\"$DEFAULT_JAVA_HOME\"\n");
+                out.write("fi\n");
 
-				out.write("exec \"$JAVA_HOME"
-					+ "/bin/java\" -mx${JAVA_HEAP_SIZE}m ${"
-					+ name.toUpperCase() + "} ");
+                out.write("exec \"$JAVA_HOME"
+                    + "/bin/java\" -mx${JAVA_HEAP_SIZE}m "+vmArgs+" ${"
+                    + name.toUpperCase() + "} ");
 
-				String jar = installDir + File.separator
-					+ name.toLowerCase() + ".jar";
+            //  String jar = installDir + File.separator
+            //      + name.toLowerCase() + ".jar";
+                
+                String jar = installDir + File.separator
+                    + name + ".jar";
 
-				out.write("-jar \"" + jar + "\" $@\n");
+                out.write("-jar \"" + jar + "\" $@\n");
 
-				out.close();
+                out.close();
 
-				// Make it executable
-				String[] chmodArgs = { "chmod", "755", script };
-				exec(chmodArgs);
-			}
-		}
+                // Make it executable
+                String[] chmodArgs = { "chmod", "755", script };
+                exec(chmodArgs);
+            }
+        }
+        
+        public class DesktopOSTask extends OSTask {
+            
+            public DesktopOSTask(Install installer) {
+                super(installer,"unix-desktop");
+            }
 
-		public class ManPageOSTask extends OSTask
-		{
-			public ManPageOSTask(Install installer)
-			{
-				super(installer,"unix-man");
-			}
+            public String getDefaultDirectory(Install installer) {
+                String dir = "/usr/local/";
+                if (!new File(dir).canWrite()) {
+                    dir = System.getProperty("user.home");
+                }
 
-			public String getDefaultDirectory(Install installer)
-			{
-				String dir = "/usr/local/";
-				if(!new File(dir).canWrite())
-					dir = System.getProperty("user.home");
+                return new File(dir,"share").getPath();
+            }
 
-				return new File(dir,"man/man1").getPath();
-			}
+            public void perform(String installDir,
+                Vector filesets) throws IOException
+            {
+                if (!enabled) {
+                    return;
+                }
 
-			public void perform(String installDir,
-				Vector filesets) throws IOException
-			{
-				if(!enabled)
-					return;
+                mkdirs(directory);
+                mkdirs(directory+"/applications");
+                mkdirs(directory+"/pixmaps");
 
-				mkdirs(directory);
+                String name = installer.getProperty("app.name");
 
-				String name = installer.getProperty("app.name");
+                // install shortcut
+                String shortcut = installer.getProperty("ostask.unix-desktop.shortcut");
+                String pixmap   = installer.getProperty("ostask.unix-desktop.pixmap");
 
-				// install man page
-				String manpage = installer.getProperty("ostask.unix-man.manpage");
+                if (shortcut != null) {
+                    InputStream in = getClass().getResourceAsStream("/" + shortcut);
+                    installer.copy(in, new File(directory+"/applications",shortcut).getPath(), null);
+                }
+                if (pixmap != null) {
+                    InputStream in = getClass().getResourceAsStream("/" + pixmap);
+                    installer.copy(in, new File(directory+"/pixmaps", pixmap).getPath(), null);
+                }
+            }
+        }
+        
+        public class ManPageOSTask extends OSTask
+        {
+            public ManPageOSTask(Install installer)
+            {
+                super(installer,"unix-man");
+            }
 
-				InputStream in = getClass().getResourceAsStream("/" + manpage);
-				installer.copy(in,new File(directory,manpage).getPath(),
-					null);
-			}
-		}
+            public String getDefaultDirectory(Install installer)
+            {
+                String dir = "/usr/local/";
+                if(!new File(dir).canWrite())
+                    dir = System.getProperty("user.home");
 
-		public OSTask[] getOSTasks(Install installer)
-		{
-			return new OSTask[] { new ScriptOSTask(installer),
-				new ManPageOSTask(installer) };
-		}
+                return new File(dir,"man/man1").getPath();
+            }
 
-		public void mkdirs(String directory) throws IOException
-		{
-			File file = new File(directory);
-			if(!file.exists())
-			{
-				String[] mkdirArgs = { "mkdir", "-m", "755",
-					"-p", directory };
-				exec(mkdirArgs);
-			}
-		}
+            public void perform(String installDir,
+                Vector filesets) throws IOException
+            {
+                if(!enabled)
+                    return;
 
-		public void exec(String[] args) throws IOException
-		{
-			Process proc = Runtime.getRuntime().exec(args);
-			proc.getInputStream().close();
-			proc.getOutputStream().close();
-			proc.getErrorStream().close();
-			try
-			{
-				proc.waitFor();
-			}
-			catch(InterruptedException ie)
-			{
-			}
-		}
-	}
+                mkdirs(directory);
 
-	public static class MacOS extends Unix
-	{
-		public String getInstallDirectory(String name, String version)
-		{
-			return "/Applications/" + name + " " + version;
-		}
-	}
+                String name = installer.getProperty("app.name");
 
-	public static class Windows extends OperatingSystem
-	{
-		public String getInstallDirectory(String name, String version)
-		{
-			return "C:\\Program Files\\" + name + " " + version;
-		}
+                // install man page
+                String manpage = installer.getProperty("ostask.unix-man.manpage");
 
-		public class JEditLauncherOSTask extends OSTask
-		{
-			public JEditLauncherOSTask(Install installer)
-			{
-				super(installer,"jedit-launcher");
-			}
+                if (manpage != null) {
+                    InputStream in = getClass().getResourceAsStream("/" + manpage);
+                    installer.copy(in, new File(directory,manpage).getPath(), null);
+                }
+            }
+        }
 
-			public String getDefaultDirectory(Install installer)
-			{
-				return null;
-			}
+        public OSTask[] getOSTasks(Install installer)
+        {
+            return new OSTask[] { new ScriptOSTask(installer),
+                new ManPageOSTask(installer), new DesktopOSTask(installer) };
+        }
 
-			public void perform(String installDir,
-				Vector filesets)
-			{
-				if(!enabled
-					|| !filesets.contains("jedit-windows"))
-					return;
+        public void mkdirs(String directory) throws IOException
+        {
+            File file = new File(directory);
+            if(!file.exists())
+            {
+                String[] mkdirArgs = { "mkdir", "-m", "755",
+                    "-p", directory };
+                exec(mkdirArgs);
+            }
+        }
 
-				// run jEditLauncher installation
-				File executable = new File(installDir,"jedit.exe");
-				if(!executable.exists())
-					return;
+        public void exec(String[] args) throws IOException
+        {
+            Process proc = Runtime.getRuntime().exec(args);
+            proc.getInputStream().close();
+            proc.getOutputStream().close();
+            proc.getErrorStream().close();
+            try
+            {
+                proc.waitFor();
+            }
+            catch(InterruptedException ie)
+            {
+            }
+        }
+    }
 
-				String[] args = { executable.getPath(), "/i",
-					System.getProperty("java.home")
-					+ File.separator
-					+ "bin" };
+    public static class MacOS extends Unix
+    {
+        public String getInstallDirectory(String name, String version)
+        {
+            return "/Applications/" + name + " " + version;
+        }
+    }
 
-				try
-				{
-					Runtime.getRuntime().exec(args).waitFor();
-				}
-				catch(IOException io)
-				{
-				}
-				catch(InterruptedException ie)
-				{
-				}
-			}
-		}
+    public static class Windows extends OperatingSystem
+    {
+        public String getInstallDirectory(String name, String version)
+        {
+            return "C:\\Program Files\\" + name + " " + version;
+        }
 
-		public OSTask[] getOSTasks(Install installer)
-		{
-			return new OSTask[] { /* new JEditLauncherOSTask(installer) */ };
-		}
-	}
+        public class JEditLauncherOSTask extends OSTask
+        {
+            public JEditLauncherOSTask(Install installer)
+            {
+                super(installer,"jedit-launcher");
+            }
 
-	public static class HalfAnOS extends OperatingSystem
-	{
-		public String getInstallDirectory(String name, String version)
-		{
-			return "C:\\" + name + " " + version;
-		}
-	}
+            public String getDefaultDirectory(Install installer)
+            {
+                return null;
+            }
 
-	public static class VMS extends OperatingSystem
-	{
-		public String getInstallDirectory(String name, String version)
-		{
-			return "./" + name.toLowerCase() + "/" + version;
-		}
-	}
+            public void perform(String installDir,
+                Vector filesets)
+            {
+                if(!enabled
+                    || !filesets.contains("jedit-windows"))
+                    return;
 
-	// private members
-	private static OperatingSystem os;
+                // run jEditLauncher installation
+                File executable = new File(installDir,"jedit.exe");
+                if(!executable.exists())
+                    return;
+
+                String[] args = { executable.getPath(), "/i",
+                    System.getProperty("java.home")
+                    + File.separator
+                    + "bin" };
+
+                try
+                {
+                    Runtime.getRuntime().exec(args).waitFor();
+                }
+                catch(IOException io)
+                {
+                }
+                catch(InterruptedException ie)
+                {
+                }
+            }
+        }
+
+        public OSTask[] getOSTasks(Install installer)
+        {
+            return new OSTask[] { /* new JEditLauncherOSTask(installer) */ };
+        }
+    }
+
+    public static class HalfAnOS extends OperatingSystem
+    {
+        public String getInstallDirectory(String name, String version)
+        {
+            return "C:\\" + name + " " + version;
+        }
+    }
+
+    public static class VMS extends OperatingSystem
+    {
+        public String getInstallDirectory(String name, String version)
+        {
+            return "./" + name.toLowerCase() + "/" + version;
+        }
+    }
+
+    // private members
+    private static OperatingSystem os;
 }
