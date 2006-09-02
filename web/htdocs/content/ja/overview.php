95a154597dd28df050652cbc77e693c79d6b0fa4
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-09-02 14:33:14 +0000

    Added Japanese translation of the overview on the front page

---
+++ web/htdocs/content/ja/overview.php
@@ -0,0 +1,9 @@
+<p>jsXe とは <b>J</b>ava  <b>S</b>imple <b>X</b>ML <b>E</b>ditorという略語です。</p>
+<p>jsXe は早くて、利用が簡単で、プラットフォーム独立のXMLエディターです。
+   <a href="http://java.sun.com">Java</a>と Swingと、
+   <a href="http://xml.apache.org/xerces2-j/">Xerces-J</a>で作成されて、
+   ライセンスは<a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License</a>.
+   jsXeの目的はユーザの簡単にXMLドキュメントを作成できるアプリケーションになることです。XMLの種類に対して、
+   いろいろなビューで、この目的を達成します。</p>
+<p>jsXeは参加者、開発者や、翻訳者や、品質保証者など、が必要です。jsXeがおもしろいと思いますなら、よかったら、
+   <?php echo create_link('get-involved.php', '参加し方'); ?>を読んでください。</p>
