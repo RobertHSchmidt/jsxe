3bb73265b29d3f82daa20c67072777fd25655854
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-09-02 15:59:17 +0000

    Updated Japanese translation

--- web/htdocs/content/ja/_global.php
+++ web/htdocs/content/ja/_global.php
@@ -10,6 +10,6 @@ $trans['ja'] = array(
     'Get Involved' => '参加情報',
     'SourceForge Project' => 'SourceForge プロジェクト',
     'jsXe with Japanese locale.' => 'jsXeは日本語も表示することができます。',
-    'A screenshot showing the context dialog including cut/paste.' => '切り取りと、コピーと、貼り付けという機能が表せるスクリーンショット。'
+    'A screenshot showing the context dialog including cut/paste.' => '切り取りと、コピーと、貼り付けという機能が表せる画面。'
 );
 ?>
