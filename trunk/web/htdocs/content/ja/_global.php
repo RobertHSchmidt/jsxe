23447605ac69c9deb82052f27b764b7ef4cffd25
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-09-02 01:45:42 +0000

    Support localization for the screenshots page

--- trunk/web/htdocs/content/ja/_global.php
+++ trunk/web/htdocs/content/ja/_global.php
@@ -8,6 +8,8 @@ $trans['ja'] = array(
     'Download' => 'ダウンロード',
     'Downloads' => 'ダウンロード',
     'Get Involved' => '参加情報',
-    'SourceForge Project' => 'SourceForge プロジェクト'
+    'SourceForge Project' => 'SourceForge プロジェクト',
+    'jsXe with Japanese locale.' => 'jsXeは日本語も表示することができます。',
+    'A screenshot showing the context dialog including cut/paste.' => '切り取りと、コピーと、貼り付けという機能が表せるスクリーンショット。'
 );
 ?>
