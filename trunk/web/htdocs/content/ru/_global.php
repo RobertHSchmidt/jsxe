569f4e131ae380b0a57c5fcb198e4dc780419a87
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-10-20 05:08:16 +0000

    added russian translation of jsxe's webpage

---
+++ trunk/web/htdocs/content/ru/_global.php
@@ -0,0 +1,19 @@
+<?php
+$trans['ru'] = array(
+    'Overview' => 'Обзор',
+    'News' => 'Новости',
+    'Features' => 'Возможности',
+    'Screenshot' => 'Скриншот',
+    'Screenshots' => 'Скриншоты',
+    'Download' => 'Скачать',
+    'Downloads' => 'Скачивание',
+    'Get Involved' => 'Помочь проекту',
+    'SourceForge Project' => 'Страница на SourceForge',
+    'jsXe with Japanese locale.' => 'jsXe с Японской локалью',
+    'A screenshot showing the context dialog including cut/paste.' => 'Этот скриншот демонстрирует диалог включающий вырезание и вставку.',
+    'A screenshot showing validation features. jsXe allows you to add nodes defined by DTD/Schema' => 'Этот скриншот демонстрирует функции проверки. jsXe позволяет вам добавлять узлы, определенные в DTD/Schema',
+    'A screenshot showing the syntax highlighted source view.' => 'Этот скриншот демонстрирует подсветку синтаксиса в отображении исходником.',
+    'A screenshot of jsXe&apos;s options panel.' => 'Это скриншот панели настроек jsXe.',
+    'A screenshot of the edit node dialog. This dialog can be used to edit nodes defined in DTD/Schema.' => 'Скриншот диалога редактора узла. Этот диалог может быть использован для редактирования узлов, определенных в DTD/Schema.'
+);
+?>
