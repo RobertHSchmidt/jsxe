bc285e84ff28ea62ad55721a3fc5c819ba0152a1
ian_lewis <ian_lewis@5ed2d69b-9114-0410-9000-eef58f7a4e40>
2006-10-23 04:22:52 +0000

    Updated Japanese translation

--- trunk/web/htdocs/content/ja/_global.php
+++ trunk/web/htdocs/content/ja/_global.php
@@ -10,6 +10,8 @@ $trans['ja'] = array(
     'Get Involved' => '参加情報',
     'SourceForge Project' => 'SourceForge プロジェクト',
     'jsXe with Japanese locale.' => 'jsXeは日本語も表示することができます。',
-    'A screenshot showing the context dialog including cut/paste.' => '切り取りと、コピーと、貼り付けという機能が表せる画面。'
+    'A screenshot showing the context dialog including cut/paste.' => '切り取りと、コピーと、貼り付けという機能が表せる画面。',
+    'A screenshot of jsXe\'s options panel. ' => 'jsXeの設定画面のスクリーンショット。',
+    'A screenshot of the edit node dialog. This dialog can be used to edit nodes defined in DTD/Schema.' => 'ノードの編集画面のスクリーンショット。'
 );
 ?>
